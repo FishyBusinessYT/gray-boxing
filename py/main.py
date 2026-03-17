0397313af119e1c98401e5af44fb20f9651c075b
Cristóbal González <cristobalj.gonzalez@gmail.com>
2026-03-17 08:31:05 -0300

    Refactor main.py

--- py/main.py
+++ py/main.py
@@ -1,55 +1,32 @@
 import cv2
-import mediapipe as mp
-from mediapipe.tasks import python
-from mediapipe.tasks.python import vision
 
-from download_task import setup
+from setup import download_task
+from capture_controller import CaptureController
+from detector import Detector
 from draw import draw_landmarks_on_image
 
-setup()
+download_task()
 
-base_options = python.BaseOptions(model_asset_path='pose_landmarker_full.task')
-options = vision.PoseLandmarkerOptions(
-    base_options=base_options,
-    running_mode=vision.RunningMode.VIDEO,
-    num_poses=1,
-    min_pose_detection_confidence=0.7,
-    min_pose_presence_confidence=0.7,
-    min_tracking_confidence=0.7,
-    )
+detector = Detector()
+captcont = CaptureController()
 
-cap = cv2.VideoCapture(0)
-fps = cap.get(cv2.CAP_PROP_FPS)
-current_frame = 0
+while True:
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-with vision.PoseLandmarker.create_from_options(options) as landmarker:
-    cv2.namedWindow("whatever")
-    cv2.moveWindow("whatever", 550, 200)
-    while True:
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+    timestamp, img, mp_image = captcont.get_frame()
+    result = detector.get_landmarks(mp_image, timestamp)
 
-        success, img = cap.read()
-        img = cv2.flip(img, 1)
-        mp_image = mp.Image(
-            image_format=mp.ImageFormat.SRGB,
-            data=cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    try:
+        annotated_image = draw_landmarks_on_image(
+            mp_image.numpy_view(), result
+            )
+        cv2.imshow("window", cv2.cvtColor(
+            annotated_image, cv2.COLOR_RGB2BGR)
             )
 
-        timestamp = int(current_frame/fps*1000)
-        landmarker_result = landmarker.detect_for_video(
-            mp_image, timestamp
-            )
+    except UnboundLocalError:
+        cv2.imshow("window", img)
 
-        try:
-            annotated_image = draw_landmarks_on_image(
-                mp_image.numpy_view(), landmarker_result
-                )
-            cv2.imshow("whatever", cv2.cvtColor(
-                annotated_image, cv2.COLOR_RGB2BGR)
-                )
-
-        except UnboundLocalError:
-            cv2.imshow("whatever", img)
-
-        current_frame += 1
+detector.cleanup()
+captcont.cleanup()
