66db243acd4e9f03496ce5f5bdf3556116f3ae56
Cristóbal González <cristobalj.gonzalez@gmail.com>
2026-04-05 12:45:03 -0300

    Removed visualization code

--- py/capture_controller.py
+++ py/capture_controller.py
@@ -16,13 +16,16 @@ class CaptureController:
         self.current_frame += 1
 
         success, img = self.capture.read()
+
+        # We flip the image to make the preview look natural
+        # Could need to remove it later
         img = cv2.flip(img, 1)
         mp_image = mp.Image(
             image_format=mp.ImageFormat.SRGB,
             data=cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
             )
 
-        return timestamp, img, mp_image
+        return timestamp, mp_image
 
     def cleanup(self):
         cv2.destroyAllWindows()
--- py/main.py
+++ py/main.py
@@ -4,7 +4,6 @@ from setup import download_task
 from capture_controller import CaptureController
 from detector import Detector
 from direction_calculator import calculate_directions
-from draw import draw_landmarks_on_image
 
 download_task()
 
@@ -13,20 +12,9 @@ captcont = CaptureController()
 
 while True:
 
-    timestamp, img, mp_image = captcont.get_frame()
-    detector_result = detector.get_landmarks(mp_image, timestamp)
-    node_directions = calculate_directions(detector_result.pose_landmarks[0])
-
-    try:
-        annotated_image = draw_landmarks_on_image(
-            mp_image.numpy_view(), detector_result
-            )
-        cv2.imshow("window", cv2.cvtColor(
-            annotated_image, cv2.COLOR_RGB2BGR)
-            )
-
-    except UnboundLocalError:
-        cv2.imshow("window", img)
+    timestamp, mp_image = captcont.get_frame()
+    landmarks = detector.get_landmarks(mp_image, timestamp).pose_landmarks[0]
+    node_directions = calculate_directions(landmarks)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         for direction_name in node_directions:
