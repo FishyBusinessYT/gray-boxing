e67288f7f771242d363e07cd4afd0be1f1974bbc
Cristóbal González <cristobalj.gonzalez@gmail.com>
2026-04-07 10:22:21 -0300

    Formatted test_receiver.gd

--- godot-project/test_receiver.gd
+++ godot-project/test_receiver.gd
@@ -11,22 +11,37 @@ var process_id := -1
 
 const DIRECTION_KEYS = [
 	"neck_head",
-	"l_shoulder_shoulder", "l_shoulder_elbow", "l_elbow_wrist", "l_wrist_hand",
-	"r_shoulder_elbow", "r_elbow_wrist", "r_wrist_hand",
-	"l_shoulder_hip", "r_shoulder_hip",
-	"l_hip_hip", "l_hip_knee", "l_knee_ankle", "l_ankle_heel", "l_ankle_toe",
-	"r_hip_knee", "r_knee_ankle", "r_ankle_heel", "r_ankle_toe",
+	"l_shoulder_shoulder",
+	"l_shoulder_elbow",
+	"l_elbow_wrist",
+	"l_wrist_hand",
+	"r_shoulder_elbow",
+	"r_elbow_wrist",
+	"r_wrist_hand",
+	"l_shoulder_hip",
+	"r_shoulder_hip",
+	"l_hip_hip",
+	"l_hip_knee",
+	"l_knee_ankle",
+	"l_ankle_heel",
+	"l_ankle_toe",
+	"r_hip_knee",
+	"r_knee_ankle",
+	"r_ankle_heel",
+	"r_ankle_toe",
 ]
 
+
 func _ready():
-	udp.bind(PORT, '127.0.0.1')
+	udp.bind(PORT, "127.0.0.1")
 	var exe_name := "gray_boxing_input.exe" if OS.get_name() == "Windows" else "gray_boxing_input"
 	var py_path := OS.get_executable_path().get_base_dir().path_join(exe_name)
 
-	if OS.has_feature('editor'):
-		py_path = ProjectSettings.globalize_path('res://').path_join('gray_boxing_input')
+	if OS.has_feature("editor"):
+		py_path = ProjectSettings.globalize_path("res://").path_join("gray_boxing_input")
 
 	process_id = OS.create_process(py_path, [], true)
+
 
 func _process(_delta):
 	if udp.get_available_packet_count() == 0:
@@ -38,13 +53,10 @@ func _process(_delta):
 
 	var raw := packet.to_float32_array()
 	for i in DIRECTION_KEYS.size():
-		directions[DIRECTION_KEYS[i]] = Vector3(
-			raw[i * 3],
-			raw[i * 3 + 1],
-			raw[i * 3 + 2]
-		)
-	
+		directions[DIRECTION_KEYS[i]] = Vector3(raw[i * 3], raw[i * 3 + 1], raw[i * 3 + 2])
+
 	$Label.text = str(directions)
+
 
 func _exit_tree() -> void:
 	if process_id != -1:
