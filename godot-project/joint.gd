54cc4bca647dddce516b344249c6c96f39975f68
Cristóbal González <cristobalj.gonzalez@gmail.com>
2026-04-12 20:45:29 -0300

    First test, it sucks

---
+++ godot-project/joint.gd
@@ -0,0 +1,19 @@
+extends RigidBody3D
+
+@export var direction_key: String
+@export var controller: PlayerController
+
+
+
+func _physics_process(delta: float) -> void:
+    if controller.directions.has(direction_key) and controller.directions[direction_key] != Vector3.ZERO:
+        var current_dir: Vector3 = global_transform.basis * Vector3(0, 1, 0)
+        var target_dir: Vector3 = controller.directions[direction_key]
+
+        var correction: Quaternion = Quaternion(current_dir, target_dir)
+
+        # Convert quaternion to axis-angle, then to angular velocity
+        var axis: Vector3 = correction.get_axis()
+        var angle: float = correction.get_angle()
+
+        angular_velocity = axis * angle * 0.5 / delta
