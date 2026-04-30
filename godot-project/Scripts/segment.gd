8ab4280b57b2f6d14a9e7c2b0082e7f7cf25468e
Cristóbal González <cristobalj.gonzalez@gmail.com>
2026-04-30 11:31:21 -0300

    A little cleanup + default values

--- godot-project/Scripts/segment.gd
+++ godot-project/Scripts/segment.gd
@@ -2,29 +2,15 @@ extends RigidBody3D
 
 @export_enum(
     "neck_head",
-    "l_shoulder_shoulder",
-    "l_shoulder_elbow",
-    "l_elbow_wrist",
-    "l_wrist_hand",
-    "r_shoulder_elbow",
-    "r_elbow_wrist",
-    "r_wrist_hand",
-    "l_shoulder_hip",
-    "r_shoulder_hip",
-    "l_hip_hip",
-    "l_hip_knee",
-    "l_knee_ankle",
-    "l_ankle_heel",
-    "l_ankle_toe",
-    "r_hip_knee",
-    "r_knee_ankle",
-    "r_ankle_heel",
-    "r_ankle_toe",
+    "l_shoulder_shoulder", "l_shoulder_elbow", "l_elbow_wrist", "l_wrist_hand",
+    "r_shoulder_elbow", "r_elbow_wrist", "r_wrist_hand", "l_shoulder_hip",
+    "r_shoulder_hip", "l_hip_hip", "l_hip_knee", "l_knee_ankle", "l_ankle_heel",
+    "l_ankle_toe", "r_hip_knee", "r_knee_ankle", "r_ankle_heel", "r_ankle_toe",
     ) var direction_key: String
 @export var controller: PlayerController
-@export var disable: bool
-@export var gain: int
 
+@export var disable := false
+@export var gain := 2
 @export var bone_axis := Vector3.UP
 
 
