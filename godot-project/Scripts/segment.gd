06e55461d02a35e55ad07aa32e6f02f4d8bb92cc
Cristóbal González <cristobalj.gonzalez@gmail.com>
2026-04-30 10:29:48 -0300

    Allow for selecting axis of alignment per-segment

--- godot-project/Scripts/segment.gd
+++ godot-project/Scripts/segment.gd
@@ -23,19 +23,21 @@ extends RigidBody3D
     ) var direction_key: String
 @export var controller: PlayerController
 @export var disable: bool
+@export var gain: int
+
+@export var bone_axis := Vector3.UP
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
     if disable: return
     if not controller.directions.has(direction_key): return
     if controller.directions[direction_key] == Vector3.ZERO: return
 
-    var current_dir: Vector3 = global_transform.basis * Vector3(0, 1, 0)
+    var current_dir: Vector3 = global_transform.basis * bone_axis
     var target_dir: Vector3 = controller.directions[direction_key]
     var correction: Quaternion = Quaternion(current_dir, target_dir)
 
     # Convert quaternion to axis-angle, then to angular velocity
     var axis: Vector3 = correction.get_axis()
     var angle: float = correction.get_angle()
-    angular_velocity = axis * angle * 0.5 / delta
+    angular_velocity = axis * angle * gain
