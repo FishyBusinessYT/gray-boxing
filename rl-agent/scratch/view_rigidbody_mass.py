680305a065683510ac5ab515a3ff8865af4e9cd7
pipev <pipe.verri@gmail.com>
2026-05-08 10:20:45 -0300

    Added legs

---
+++ rl-agent/scratch/view_rigidbody_mass.py
@@ -0,0 +1,12 @@
+import mujoco
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).parents[1].resolve()
+
+model = mujoco.MjModel.from_xml_path(str(ROOT_DIR / "assets/mujoco_envs/openai_humanoid.xml"))
+
+# 1. Ver todas las masas con sus nombres
+for i in range(model.nbody):
+    nombre = mujoco.mj_id2name(model, mujoco.mjtObj.mjOBJ_BODY, i)
+    masa = model.body_mass[i]
+    print(f"Cuerpo: {nombre} | Masa: {masa:.4f} kg")
